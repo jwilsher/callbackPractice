cbd7ae612c91446cd5d96b0bad8d303d6c1f1ef2
Jaclyn Cowley <jaclyncowley11@Jaclyns-MacBook-Pro-2.local>
2016-03-02 21:20:16 -0700

    Lots of callback practice

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,4 +1,5 @@
-/* In this repo your job is to write functions to make each function call work properly.
+/* In this repo your job is to write functions to make each function call work 
+properly.
 Below is a sample problem 
 
   //code here for sayHi
@@ -25,6 +26,12 @@ and what you should write is the sayHi function that makes the code above work,
 
 
   //Code Here for first
+
+function first(arr, cb) {
+  return cb(arr[0]);
+}
+
+
   
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -40,6 +47,12 @@ first(names, function(firstName){
 
 
   //Code Here for last
+
+function last(arr, cb) {  //look at what the last function is expecting as parameters
+  return cb(arr[arr.length-1]);   // look at what the function call does below....then look at 
+                          //the callback function and put in the "cb" whatever the fuctnion
+                          //execution below is expecting
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
@@ -59,6 +72,11 @@ last(names, function(lastName){
 
   //Code Here for multiply
 
+
+function multiply(num1, num2, cb) {
+  return cb(num1 * num2);
+}
+
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
 })
@@ -74,6 +92,19 @@ multiply(4, 3, function(answer){
 
 
   //Code Here for contains
+
+  ///STUDY THIS!!!!
+
+function contains(arr, str, cb) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === str) {
+          return cb(true);
+        }
+          
+    }
+    return cb(false);
+}
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -95,6 +126,35 @@ contains(names, 'Colt', function(result){
 
     //Code Here for uniq
 
+
+
+/*
+IN CLASS....
+
+function uniq(collection, fn) {
+    var obj = {};
+    var uniqArr = [];
+    collection.forEach(function(item) {
+      obj[item] = item;
+    });
+    for (var key in obj) {
+      uniqArr.push(key);
+    }
+    return fn(uniqArr);
+}
+*/
+
+function uniq(arr, cb) {
+  resultArr = [];
+  arr.forEach(function(ele, index) {
+    if (resultArr.indexOf(ele) === -1) {
+      resultArr.push(ele);
+    }
+  });
+  return cb(resultArr);
+}
+
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -110,6 +170,14 @@ uniq(names, function(uniqArr){
 
 
     //Code Here for each
+
+function each(arr, cb) {
+  arr.forEach(function(ele, index) {
+    return cb(ele, index);
+  });
+}
+
+
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
@@ -127,6 +195,19 @@ each(names, function(item, indice){
 
 
  //code here for getUserById
+
+function getUserById(arr, str, cb) {
+  console.log(cb); /* at this point cb is equal to the function definition that you passed in...which was.....function(user) {
+                       console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); })*/
+  arr.forEach(function(obj, index) {
+    if (obj.id === str) {
+      return cb(obj);   // now cb (which is set to the definition of another function) is called witH the parameter obj....THIS IS WHERE THE CODE EXECUTES!!!
+    }
+  });
+}
+
+
+
 
 var users = [
   {
@@ -149,6 +230,21 @@ var users = [
   },
 ];
 
-getUserById(users, '16t', function(user){
+getUserById(users, '16t', function(user) {
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
-});
+});  //getUsersById is NOT calling the function that was passed into it....it is JUST passing in the FUNCTION DEFINITION...which it then refers
+    //back to the function that was defined here (ie getUsersById) and uses this FUNCTION DEFINITION as the "cb" that gets passed in
+
+
+/*
+GIVING THE FUNCTION A NAME MAY HELP THE ABOVE CODE MAKE MORE SENSE...
+function logUser (user) {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+}
+
+getUserById(users, "16t", logUser; */
+
+
+
+
+
